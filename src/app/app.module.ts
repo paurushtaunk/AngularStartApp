7daee19587784fc45e7c95c9942d1a3c9aa72da7
Paurush M Taunk <PaurushM.Taunk@thomsonreuters.com>
2020-05-25 18:57:30 +0530

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { FooterCopyrightsComponent } from './footer-copyrights/footer-copyrights.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    FooterCopyrightsComponent
+    FooterCopyrightsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+    // { path: 'dashboard/:dashboard', component: DashboardComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.qed.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    Qed: true
+  };
